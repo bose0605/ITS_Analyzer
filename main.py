5244ba4aa9e16aeafc8e741d366035fdaa2992bf
bose0605 <sbs060586@naver.com>
2025-04-18 14:11:46 +0900

    Update main.py

--- main.py
+++ main.py
@@ -12,5 +12,5 @@ st.set_page_config(
 st.title("📂 ITS Tool Menu")
 
     # sensor correlation 링크
-    st.page_link("pages/sensor correlation.py", label="📊 sensor correlation")
-    st.page_link("pages/pTAT-viewer app.py", label="📈 pTAT Viewer")
+st.page_link("pages/sensor correlation.py", label="📊 sensor correlation")
+st.page_link("pages/pTAT-viewer app.py", label="📈 pTAT Viewer")
